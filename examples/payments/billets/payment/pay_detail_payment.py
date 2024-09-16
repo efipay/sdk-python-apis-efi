20b96a56b78ab699489ca126cef5620871c6b9a1
Palloma Brito <palloma.brito@sejaefi.com.br>
2024-09-16 14:43:46 -0300

    :sparkles: Exemplo para consultar solicitação de pagamento

---
+++ examples/payments/billets/payment/pay_detail_payment.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+from efipay import EfiPay
+from ....credentials import credentials
+
+efi = EfiPay(credentials.CREDENTIALS)
+
+params = {
+    'idPagamento': 1
+}
+
+response = efi.pay_detail_payment(params=params)
+print(response)
