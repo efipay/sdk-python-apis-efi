854c148b760020b755543d7a2008b05d9b21f3c8
Palloma Brito <palloma.brito@sejaefi.com.br>
2024-09-16 14:46:55 -0300

    :sparkles: Exemplo para criar lote de cobranças com vencimento

---
+++ examples/pix/batch/cobv/pix_create_due_charge_batch.py
@@ -0,0 +1,51 @@
+# encoding: utf-8
+
+from efipay import EfiPay
+from ....credentials import credentials
+
+efi = EfiPay(credentials.CREDENTIALS)
+
+params = {
+    'id': 1
+}
+
+body = {
+  "descricao": "Cobranças dos alunos do turno vespertino",
+  "cobsv": [
+    {
+      "calendario": {
+          "dataDeVencimento": "2024-11-28",
+          "validadeAposVencimento": 30
+      },
+      "txid": "fb2761260e55ood593c7926bjb5cb650",
+      "devedor": {
+          "cpf": "01234567890",
+          "nome": "João Souza"
+      },
+      "valor": {
+          "original": "100.50"
+      },
+      "chave": "a572c113-7d13-49ba-9988-0yy7e061a356",
+      "solicitacaoPagador": "Informar matrícula"
+      },
+      {
+      "calendario": {
+          "dataDeVencimento": "2020-12-31",
+          "validadeAposVencimento": 30
+      },
+      "txid": "7978c0c97ea84ppe78e8849634473c9f1",
+      "devedor": {
+          "cpf": "15311295449",
+          "nome": "Manoel Silva"
+      },
+      "valor": {
+          "original": "100.50"
+      },
+      "chave": "a572c113-7d13-49ba-9988-0yy7e061a356",
+      "solicitacaoPagador": "Informar matrícula"
+    }
+  ]
+}
+
+response = efi.pix_create_due_charge_batch(params=params, body=body)
+print(response)
