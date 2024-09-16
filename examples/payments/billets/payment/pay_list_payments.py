79a5fa2e9bfab8f164203210efbf46611f0cf1c8
Palloma Brito <palloma.brito@sejaefi.com.br>
2024-09-16 14:44:10 -0300

    :sparkles: Exemplo para consultar lista de solicitações de pagamento

---
+++ examples/payments/billets/payment/pay_list_payments.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+from efipay import EfiPay
+from ....credentials import credentials
+
+efi = EfiPay(credentials.CREDENTIALS)
+
+params = {
+    'dataInicio': '2020-10-22',
+    'dataFim': '2022-06-23'
+}
+
+response = efi.pay_list_payments(params=params)
+print(response)
