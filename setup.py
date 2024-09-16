21e7571ea659722261d1b959bee8c6ad4c1da832
Palloma Brito <palloma.brito@sejaefi.com.br>
2024-09-16 14:11:07 -0300

    :bookmark: Atualização de versão

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ dependencies = [dependency.strip()
 setup(
     name='efipay',
 
-    version='1.0.1',
+    version='1.0.2',
 
     description='Module for integration with EfiPay API',
     long_description=long_description,
