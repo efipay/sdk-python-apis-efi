ba7139e2a4d168177313eca7df167e03dcde18f4
Palloma Brito <palloma.brito@sejaefi.com.br>
2024-09-16 14:44:33 -0300

    :sparkles: Exemplo para solicitar pagamento de código de barras

---
+++ examples/payments/billets/payment/pay_request_bar_code.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+from efipay import EfiPay
+from ....credentials import credentials
+
+efi = EfiPay(credentials.CREDENTIALS)
+
+params = {
+    'codBarras': 1
+}
+
+body = {
+    'valor': 9300,
+    'dataPagamento': '2022-06-17',
+    'descricao': 'Pagamento de boleto, teste API Pagamentos'
+}
+
+response = efi.pay_request_bar_code(params=params, body=body)
+print(response)
