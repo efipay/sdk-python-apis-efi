96fa9214a2b863365878afff8a32e1ccdc5271ea
Palloma Brito <palloma.brito@sejaefi.com.br>
2025-06-13 12:48:08 -0300

    ✨ Métodos utilizados na Jornada 3

---
+++ examples/pix/automatic/jornada3/pix_create_immediate_charge.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+from efipay import EfiPay
+from ...credentials import credentials
+
+efi = EfiPay(credentials.CREDENTIALS)
+
+body = {
+    'calendario': {
+        'expiracao': 3600
+    },
+    'devedor': {
+        'cpf': '',
+        'nome': ''
+    },
+    'valor': {
+        'original': ''
+    },
+    'chave': '',
+    'solicitacaoPagador': 'Cobrança dos serviços prestados.'
+}
+
+response =  efi.pix_create_immediate_charge(body=body)
+print(response)
