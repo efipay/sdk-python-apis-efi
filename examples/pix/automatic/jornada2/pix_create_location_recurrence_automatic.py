bcf2d57f70c17e4a6ef900100ba41ffa4dc237e1
Palloma Brito <palloma.brito@sejaefi.com.br>
2025-06-13 15:51:46 -0300

    ✨ Exemplo para Criar location do payload de recorrência de Pix Automático

--- examples/pix/automatic/jornada2/pix_create_location_recurrence_automatic.py
+++ examples/pix/automatic/jornada2/pix_create_location_recurrence_automatic.py
@@ -5,12 +5,6 @@ from ....credentials import credentials
 
 efi = EfiPay(credentials.CREDENTIALS)
 
-body = {
-  "id": 12069,
-  "location": "pix.example.com/qr/v2/rec/2353c790eefb11eaadc10242ac120002",
-  "criacao": "2023-12-20T12:38:28.774Z"
-}
-
-response =  efi.pix_create_location_recurrence_automatic(body=body)
+response =  efi.pix_create_location_recurrence_automatic()
 print(response)
 
